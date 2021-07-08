55f71850a4b808fbc663ad1340e2fa03bd399631
Nicolas Fodor <nicolasfodor@xv1.lan>
2021-07-08 11:32:01 -0700

    Automatic Ghost configuration when using ngrok URL

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ set +x #set to -x to trace this script on the console.
     NGROK_HOSTNAME=$(curl --silent --show-error http://0.0.0.0:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
     echo "${bold}Ghosty is now starting Ghost on https://$NGROK_HOSTNAME"
     set -e #Quit if yarn issues.
-    nohup yarn start > ghosty.log > /dev/null &
+    url="https://${NGROK_HOSTNAME}" nohup yarn start > ghosty.log > /dev/null &
     sleep 1 
     echo "${bold}Ghosty's mission complete, Ghost is listening on https://$NGROK_HOSTNAME. Control C to exit."
 
