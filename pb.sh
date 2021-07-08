b93d0578e154ecd7e8712431f376c7f3e55f0d2e
Nicolas Fodor <nicolasfodor@xv1.lan>
2021-07-08 13:34:07 -0700

    Changed Docker naming for docker.io push and availability.

---
+++ pb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#This script will build the docker file and run Ghostly immediately after.
+docker build . -t invictieu/ghosty:0.1.0
+docker run -e "NGROK=1" -e "GHOST_HOSTNAME=" -ti  -p 2368:2368 -p 5555:5555 invictieu/ghosty:0.1.0 sh
