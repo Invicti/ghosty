618043751f7252e026d127ac4f09279506c78704
Nicolas Fodor <nicolasfodor@xv1.lan>
2021-07-08 14:25:19 -0700

    Addded ability to push to docker.io in  startup scripts

--- pb.sh
+++ pb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #This script will build the docker file and run Ghostly immediately after.
-docker build . -t invictieu/ghosty:0.1.0 -t latest
-docker push invictieu/ghosty:0.1.0 
-docker run -e "NGROK=1" -e "GHOST_HOSTNAME=" -ti  -p 2368:2368 -p 5555:5555 invictieu/ghosty:0.1.0 sh
+PUSH_REPOSITORY=docker.io
+IMAGE=invictieu/ghosty
+VERSION=0.1.0
+docker build . -t ${IMAGE}:${VERSION} -t ${IMAGE}:latest
+docker push ${PUSH_REPOSITORY}/${IMAGE}:${VERSION}
+docker push ${PUSH_REPOSITORY}/${IMAGE}:latest
+docker run -e "NGROK=1" -e "GHOST_HOSTNAME=" -ti  -p 2368:2368 -p 5555:5555 ${PUSH_REPOSITORY}/${IMAGE}:${VERSION} sh
--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #This script will build the docker file and run Ghostly immediately after.
-docker build . -t invicti/ghosty:main
-docker run -e "NGROK=1" -e "GHOST_HOSTNAME=" -ti  -p 2368:2368 -p 5555:5555 invicti/ghosty:main sh
+#!/bin/bash
+#This script will build the docker file and run Ghostly immediately after.
+PUSH_REPOSITORY=docker.io
+IMAGE=invictieu/ghosty
+VERSION=0.1.0
+docker build . -t ${IMAGE}:${VERSION} -t ${IMAGE}:latest
+docker run -e "NGROK=1" -e "GHOST_HOSTNAME=" -ti  -p 2368:2368 -p 5555:5555 ${PUSH_REPOSITORY}/${IMAGE}:${VERSION} sh
